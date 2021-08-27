ef6da7589c64f69e1477a40603baf09ef29486fb
mvencel14 <85483029+mvencel14@users.noreply.github.com>
2021-08-27 15:46:20 +0200

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -12,9 +12,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 # echo root:temp1234 | chpasswd # if you want to have a password for root user
 
-pacman -S grub efibootmgr lvm2 networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font zip unzip unrar p7zip htop man-db man-pages pacman-contrib fwupd
-# Alternate to pulseaudio packages
-# pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack
+pacman -S grub efibootmgr lvm2 networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font zip unzip unrar p7zip htop man-db man-pages pacman-contrib fwupd pipewire pipewire-alsa pipewire-pulse pipewire-jack tlp vnstat
 
 # If you want to install KDE Plasma desktop environment
 # pacman -S xorg xorg-server sddm plasma kwalletmanager konsole filelight ark gwenview kate okular spectacle dolphin kcalc simplescreenrecorder discover packagekit-qt5
@@ -22,11 +20,7 @@ pacman -S grub efibootmgr lvm2 networkmanager network-manager-applet dialog wpa_
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-# pacman -S --noconfirm xf86-video-intel
-
-# If you want to install on laptop
-# pacman -S tlp
-# systemctl enable tlp
+# No package needed for intel cards
 
 # If you want to install on virtualbox
 # pacman -S virtualbox-guest-utils
@@ -40,11 +34,15 @@ sed -i '33s/.//' /etc/pacman.conf
 sed -i '93s/.//' /etc/pacman.conf
 sed -i '94s/.//' /etc/pacman.conf
 sed -i '22 i --country Hungary' /etc/xdg/reflector/reflector.conf
+pacman -Syy
 
 # If esp is mounted under /efi
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# systemctl enable tlp
+systemctl enable paccache.timer
+systemctl enable vnstat.service
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
