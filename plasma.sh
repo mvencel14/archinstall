fc2d4cc3a1f562d1372767e2ac23f676d081e650
mvencel14 <85483029+mvencel14@users.noreply.github.com>
2021-09-12 12:29:08 +0200

    Add files via upload

---
+++ plasma.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pacman -S xorg xorg-server xorg-xinit sddm plasma plasma-wayland-session plasma-wayland-protocols kwalletmanager konsole filelight ark gwenview kate okular spectacle dolphin kcalc simplescreenrecorder discover packagekit-qt5 firefox dmidecode libreoffice smplayer smplayer-{skins,themes} smtube mpv qbittorrent kdenlive
