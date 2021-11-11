a215bef717fdb154d1d1c4da985b080bdbdf6697
mvencel14 <85483029+mvencel14@users.noreply.github.com>
2021-11-11 17:44:24 +0100

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 pacman -S xorg xorg-server xorg-xinit sddm plasma plasma-wayland-session plasma-wayland-protocols kwalletmanager konsole filelight ark gwenview kate okular spectacle dolphin kcalc simplescreenrecorder discover packagekit-qt5 firefox dmidecode libreoffice smplayer smplayer-{skins,themes} smtube mpv qbittorrent kdenlive
+echo '[X11]' >> /etc/sddm.conf
+echo 'ServerArguments=-nolisten tcp -dpi 97' >> /etc/sddm.conf
