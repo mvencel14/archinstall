e6d9d5bc0e746ac638d7b5f605380c44849531cf
mvencel14 <85483029+mvencel14@users.noreply.github.com>
2022-02-21 18:19:40 +0100

    Add files via upload

---
+++ xfce.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter xorg-server xorg-xinit lightdm-slick-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox simplescreenrecorder arc-gtk-theme arc-icon-theme papirus-icon-theme inter-font mpv qbittorrent vlc
